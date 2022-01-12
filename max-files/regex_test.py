a0baee4f9ed8d75b9f6c232e53d4765bcfd8b4be
maxxie114 <maxxie114@gmail.com>
2022-01-12 04:43:34 +0000

    try some NLP with regex to do paraphrasing

--- max-files/regex_test.py
+++ max-files/regex_test.py
@@ -1,9 +1,8 @@
-# A program that filter out all of the urls in a paragraph
 import re
 
 # A paragraph that contains some website
 message = ("If you need help with programming, please look up your answer "
-        "on https://www.google.com/, "
+           "on https://www.google.com/, "
            "If you need to look up information on every single built-in "
            "function in python, please visit "
            "https://docs.python.org/3.8/library/index.html/, if you want to "
@@ -16,12 +15,25 @@ message = ("If you need help with programming, please look up your answer "
 
 # A regular expression I made that can filter out most of the urls in ending in com,org,net,gg
 regex = r"((https|http):\/\/[a-zA-Z0-9.]+?[a-zA-Z0-9]+\.(com|org|net|gg)[a-zA-Z0-9./]+\/?)"
+# regex for finding the purpose of a url
+find_context = r"(if[\w\s-]+(need|want)[\w\s-]+)"
 url_list = re.findall(regex, message)
-# This will print everything that the regex captured
-print(url_list)
-# list of issues
-issues = ["need help with programming", "need to look up python documentation",
-          "need help with python issue", "need to ask question about python"]
-# range(4) will generate a list of numbers between 0 and 4
-for i in range(4):
-    print(f"{issues[i]}: {url_list[i][0]}")
+context_list = re.findall(find_context, message, re.IGNORECASE)
+
+# nlp stuff
+filtered_context_sentences = []
+context_sentences = []
+stopwords = ["if","you","with","every","the","single","world","on","to","in","an","of","about","them","want","need","look","up"]
+for sentence in context_list:
+    lowered_sentence = sentence[0].lower()
+    context_sentences.append(lowered_sentence)
+    temp = lowered_sentence.split(" ")
+    # filter out stopwords
+    filtered_context_list = [words for words in temp if words not in stopwords]
+    # insert a "need" at the front of the sentence
+    filtered_context_list.insert(0, "need")
+    filtered_context_sentences.append(" ".join(filtered_context_list))
+
+# use len() just in case if there are more than 4 urls
+for i in range(len(url_list)):
+    print(f"{filtered_context_sentences[i]}: {url_list[i][0]}")
